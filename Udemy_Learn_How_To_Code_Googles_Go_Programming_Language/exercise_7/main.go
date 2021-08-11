e45cfcdfd9f11a841f7ad31fe46eeb19bfc375b0
Gui Lopes <gui.lesteves@gmail.com>
2021-08-11 09:56:15 +0100

    ninja level 2, exercise 3

--- Udemy_Learn_How_To_Code_Googles_Go_Programming_Language/exercise_7/main.go
+++ Udemy_Learn_How_To_Code_Googles_Go_Programming_Language/exercise_7/main.go
@@ -6,8 +6,14 @@ import (
 
 func main() {
 	x := 33
+	const y int = 36
 
 	fmt.Printf("My number in decimal is %d\n", x)
 	fmt.Printf("My number in binary is %b\n", x)
 	fmt.Printf("My number in decimal is %#x\n", x)
+
+	fmt.Printf("My number in decimal is %d\n", y)
+	fmt.Printf("My number in binary is %b\n", y)
+	fmt.Printf("My number in decimal is %#x\n", y)
+
 }
