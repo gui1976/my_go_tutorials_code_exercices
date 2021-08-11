df9b9a7fa6ff288fa81df824463e75be999d5938
Gui Lopes <gui.lesteves@gmail.com>
2021-08-11 09:08:22 +0100

    nija level 2 exercise 1

---
+++ Udemy_Learn_How_To_Code_Googles_Go_Programming_Language/exercise_7/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 33
+
+	fmt.Printf("My number in decimal is %d\n", x)
+	fmt.Printf("My number in binary is %b\n", x)
+	fmt.Printf("My number in decimal is %#x\n", x)
+}
