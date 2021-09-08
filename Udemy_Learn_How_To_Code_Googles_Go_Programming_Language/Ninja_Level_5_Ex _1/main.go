7fac9becce7c4c11d552e3b696aad80c773f81a2
Gui Lopes <gui.lesteves@gmail.com>
2021-09-08 14:49:23 +0100

    Ninja_Level_5_Ex _1

---
+++ Udemy_Learn_How_To_Code_Googles_Go_Programming_Language/Ninja_Level_5_Ex _1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	first_name        string
+	last_name         string
+	ice_cream_flavors []string
+}
+
+func printPerson(pp *person) {
+	fmt.Printf("The ice cream flavours that %s %s likes are", pp.first_name, pp.last_name)
+	for _, flavour := range pp.ice_cream_flavors {
+		fmt.Printf(" %s\t", flavour)
+	}
+	fmt.Printf("\n")
+}
+
+func main() {
+
+	p1 := person{
+		first_name:        "Gui",
+		last_name:         "Lopes",
+		ice_cream_flavors: []string{"Ananas", "Coffee", "strawberry"},
+	}
+
+	p2 := person{
+		first_name:        "Alex",
+		last_name:         "Esteves",
+		ice_cream_flavors: []string{"Chocolate", "Mango", "Strawberry"},
+	}
+
+	printPerson(&p1)
+	printPerson(&p2)
+}
